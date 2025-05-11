08e7dde5a34b45ff1f6863ac30d1eaa60a04b267
luizbarueri <luizbarueri@gmail.com>
2025-05-10 23:28:51 -0300

    usando javascript

---
+++ script.js
@@ -0,0 +1,35 @@
+//var url = document.querySelector('a').href
+var flame = document.querySelector('iframe')
+
+function abreVideo(x) {
+    if  (x == 1) {
+        flame.src = "https://www.youtube.com/embed/VG8ciFpwwcQ?list=RDVG8ciFpwwcQ"
+    }
+    if  (x == 2) {
+        flame.src = "https://www.youtube.com/embed/fgTaIOoXr3s?list=RDfgTaIOoXr3s"
+    }
+    if  (x == 3) {
+        flame.src = "https://www.youtube.com/embed/RnDDQ9CY-1I?list=RDfgTaIOoXr3s"
+    }
+    if  (x == 4) {
+        flame.src = "https://www.youtube.com/embed/0lMFS5Ji4AE?list=RDfgTaIOoXr3s"
+    }
+    if  (x == 5) {
+        flame.src = "https://www.youtube.com/embed/w46bWxS9IjY?list=RDfgTaIOoXr3s"
+    }
+    if  (x == 6) {
+        flame.src = "https://www.youtube.com/embed/3PvH6d6rpXw?list=RDfgTaIOoXr3s"
+    }
+    if  (x == 7) {
+        flame.src = "https://www.youtube.com/embed/xhv4f6FjWNM?list=RDxhv4f6FjWNM"
+    }
+    if  (x == 8) {
+        flame.src = "https://www.youtube.com/embed/6NXnxTNIWkc?list=RDxhv4f6FjWNM"
+    }
+    if  (x == 9) {
+        flame.src = "https://www.youtube.com/embed/vjAGt3IpLVk?list=RDvjAGt3IpLVk"
+    }
+    if  (x == 10) {
+        flame.src = "https://www.youtube.com/embed/Bquy7lBfVTU"
+    }
+}
